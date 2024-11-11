09153498e30585332a7b5e1fd0a8042b4e0c8e46
woohyuk <7dngur7@naver.com>
2024-11-11 22:52:17 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/medilux/moasis/MoasisApplication.java
@@ -0,0 +1,13 @@
+package medilux.moasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/medilux/moasis/MoasisApplicationTests.java
@@ -0,0 +1,13 @@
+package medilux.moasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
